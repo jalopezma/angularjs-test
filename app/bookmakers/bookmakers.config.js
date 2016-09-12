d1ac9eec39487b5b7c1792b13cdf456882e1cb7b
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2016-09-12 20:26:52 +0100

    Initialize bookmakers module

---
+++ app/bookmakers/bookmakers.config.js
@@ -0,0 +1,15 @@
+(function() {
+    angular
+        .module('app.bookmakers')
+        .config(config);
+
+    function config($routeProvider) {
+        $routeProvider
+            //.when('/bookmakers/:bookmaker', {
+                //templateUrl: 'bookmakers/bookmaker.html',
+                //controller: 'BookmakerController',
+                //controllerAs: 'vm'
+            //})
+            ;
+    }
+})();
